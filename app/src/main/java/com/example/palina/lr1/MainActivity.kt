640d7513db4e3ebcd48eca3b0596349bddb2540e
Palina Sh <palina.sh@gmail.com>
2018-10-09 13:20:41 +0300

    Added READ_STATE_PHONE permission

--- app/src/main/java/com/example/palina/lr1/MainActivity.kt
+++ app/src/main/java/com/example/palina/lr1/MainActivity.kt
@@ -2,11 +2,72 @@ package com.example.palina.lr1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.Manifest
+import android.content.pm.PackageManager
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AlertDialog
+
+
 
 class MainActivity : AppCompatActivity() {
+    private val PERMISSIONS_REQUEST_READ_PHONE_STATE: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getDeviceIMEI()
+    }
+
+    private fun getDeviceIMEI(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.READ_PHONE_STATE)) {
+                val dialog = AlertDialog.Builder(this)
+                dialog.setMessage(resources.getString(R.string.permissionText))
+                dialog.setTitle(resources.getString(R.string.permissionTitle))
+                dialog.setCancelable(false)
+                dialog.setPositiveButton("OK") { dialogInterface, which ->
+                    ActivityCompat.requestPermissions(this,
+                            arrayOf(Manifest.permission.READ_PHONE_STATE),
+                            PERMISSIONS_REQUEST_READ_PHONE_STATE)
+                }
+                dialog.show()
+            }
+            else {
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.READ_PHONE_STATE),
+                        PERMISSIONS_REQUEST_READ_PHONE_STATE)
+            }
+        }
+        else {
+            showIMEI()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_READ_PHONE_STATE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    showIMEI()
+                } else {
+                    //
+                }
+                return
+            }
+
+
+        }
+    }
+
+    private fun showIMEI(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                !=PackageManager.PERMISSION_GRANTED)
+            return
+
     }
 }
